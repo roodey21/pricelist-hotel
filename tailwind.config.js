3c8793e1dd686779b979e64d36d1e73f8f08afcc
roodey21 <fahrudin.abadi@students.amikom.ac.id>
2022-10-18 14:48:55 +0700

    lgh done, nunggu logo yang terupdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
+    "./lch/**/*.{html,js}",
+    "./lgh/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
